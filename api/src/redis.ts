1ccc93f280624ccea7c31721ffd5e6ae887ec938
Rithik M <rithik7899@gmail.com>
2025-02-05 09:38:01 +0530

    docker files

--- api/src/redis.ts
+++ api/src/redis.ts
@@ -1,25 +1,25 @@
 import { createClient } from 'redis'
 
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 
-export const subscriber = createClient()
-const client = createClient()
-
+export const subscriber = createClient({ url: REDIS_URL });
+const client = createClient({ url: REDIS_URL });
 
 const connectRedis = async () => {
     try {
         await subscriber.connect();
         await client.connect()
         console.log("connected to redis")
-    } catch(e) {
-        console.error("failed to connect redis",e)
+    } catch (e) {
+        console.error("failed to connect redis", e)
     }
 }
 
-export const queuePush = async (queueName:string, data:string) => {
-    try{
+export const queuePush = async (queueName: string, data: string) => {
+    try {
         await client.lPush(queueName, data);
-    } catch(e) {
-        console.log("failed to push to queue",e)
+    } catch (e) {
+        console.log("failed to push to queue", e)
     }
 }
 
